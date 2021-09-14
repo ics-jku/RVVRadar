560a1477b974882885e24563fab4ba357a6fa63a
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2021-09-14 16:03:43 +0200

    bmark_memcpy: fixed parameters (order)

--- bmark_memcpy_c_avect.c
+++ bmark_memcpy_c_avect.c
@@ -1,5 +1,5 @@
 
-void memcpy_c_avect_byte(char *src, char *dest, unsigned int len)
+void memcpy_c_avect_byte(char *dest, char *src, unsigned int len)
 {
 	for (int i = 0; i < len; i++)
 		dest[i] = src[i];
--- bmark_memcpy_c_noavect.c
+++ bmark_memcpy_c_noavect.c
@@ -1,5 +1,5 @@
 
-void memcpy_c_noavect_byte(char *src, char *dest, unsigned int len)
+void memcpy_c_noavect_byte(char *dest, char *src, unsigned int len)
 {
 	for (int i = 0; i < len; i++)
 		dest[i] = src[i];
