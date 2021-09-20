20ea240c2982af50491326be7d9adab92a1f84a4
Manfred SCHLAEGL <manfred.schlaegl@gmx.at>
2021-09-20 12:13:45 +0200

    simplified main programm (no program arguments now)

--- rvvbmark.c
+++ rvvbmark.c
@@ -14,40 +14,17 @@
 #include "bmark_memcpy.h"
 
 
+/* TODO: implement programm parameters for this */
+#define ENABLE_MEMCPY	1
+#define ITERATIONS	100
 #define LEN_START	128
-#define LEN_END		1024 * 1024 * 16
-
-int memcpybmarkbmark()
-{
-	int ret = 0;
-
-	bmarkset_t *bmarkset = bmarkset_create("rvvbmark");
-	if (bmarkset == NULL)
-		return -1;
-
-	/* 128 byte to 16MiB, doubling the size after every iteration */
-	for (int len = LEN_START; len <= LEN_END; len <<= 1)
-		if (bmark_memcpy_add(bmarkset, len) < 0) {
-			goto __ret_bmarkset_destroy;
-			return -1;
-		}
-
-	bmarkset_reset(bmarkset);
-
-	bmarkset_run(bmarkset, 0, 1000, true);
-
-	ret = 0;
-
-__ret_bmarkset_destroy:
-	bmarkset_destroy(bmarkset);
-	return ret;
-}
-
-
+#define LEN_END		1024*1024*16
 
 
 int main(int argc, char **argv)
 {
+	int ret = 0;
+
 	fprintf(stderr, "%s (C) 2021 Manfred Schlaegl <manfred.schlaegl@gmx.at>\n", RVVBMARK_VERSION_STR);
 
 	fprintf(stderr, "RISC-V support is ");
@@ -65,7 +42,27 @@ int main(int argc, char **argv)
 	fprintf(stderr, "disabled\n");
 #endif /* RVVBMARK_RV_SUPPORT */
 
-	memcpybmarkbmark();
 
-	return 0;
+	bmarkset_t *bmarkset = bmarkset_create("rvvbmark");
+	if (bmarkset == NULL)
+		return -1;
+
+	/* 128 byte to 16MiB, doubling the size after every iteration */
+	for (int len = LEN_START; len <= LEN_END; len <<= 1) {
+#if ENABLE_MEMCPY
+		if (bmark_memcpy_add(bmarkset, len) < 0) {
+			ret = -1;
+			goto __ret_bmarkset_destroy;
+		}
+#endif /* ENABLE_MEMCPY */
+	}
+
+	bmarkset_reset(bmarkset);
+	bmarkset_run(bmarkset, 0, ITERATIONS, true);
+
+	ret = 0;
+
+__ret_bmarkset_destroy:
+	bmarkset_destroy(bmarkset);
+	exit(ret);
 }
